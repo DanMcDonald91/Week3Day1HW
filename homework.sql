8fb6b9f2c2eb43e241b37a3f4d40cbbe33e1ff42
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-01-23 21:02:01 +0000

    Database Homework

---
+++ homework.sql
@@ -0,0 +1,17 @@
+SELECT * FROM movies;
+
+-- SELECT people.name FROM people;
+
+-- UPDATE people SET name = 'Alastair Hood' WHERE name = 'Allisder Hood';
+-- SELECT name FROM people WHERE id = 10;
+
+-- DELETE FROM movies WHERE title = 'Batman Begins';
+
+
+-- INSERT into people (name) VALUES (' John McColum');
+
+-- DELETE FROM people WHERE name = 'Harvey Wheaton'
+
+-- INSERT into movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2017, '22.30');
+
+UPDATE movies SET show_time = '21.30' WHERE title = 'Guardians of the Galaxy';
